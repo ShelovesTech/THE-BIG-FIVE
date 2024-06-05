477de7c2702fc25287a9f0e8a8a16f567d598f7f
Kevin Isom <47275702+Isomkevin@users.noreply.github.com>
2024-06-05 15:29:03 +0000

    app update

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-
-# *384*31278#
-import os
-from flask import Flask, request
+from flask import Flask, request, Response
 from africastalking.AfricasTalking import AfricasTalking, Voice
-from dotenv import load_dotenv
+import os
 
+# Load environment variables
+from dotenv import load_dotenv
+load_dotenv()
+
+# Create Flask App
+app = Flask(__name__)
+PORT = int(os.environ.get('PORT', 3000))
 
 # Configure Africa's Talking credentials
 username = os.environ.get('AT_USERNAME')
@@ -12,45 +16,6 @@ api_key = os.environ.get('AT_API_KEY')
 AT = AfricasTalking(username, api_key)
 sms = AT.Messaging
 voice = Voice
-
-# Load environment variables
-load_dotenv()
-
-# Create Flask App
-app = Flask(__name__)
-PORT = int(os.environ.get('PORT', 3000))
-
-response = ""
-
-@app.route('/', methods=['POST', 'GET'])
-def ussd_callback():
-    global response
-    session_id = request.values.get("sessionId", None)
-    service_code = request.values.get("serviceCode", None)
-    phone_number = request.values.get("phoneNumber", None)
-    text = request.values.get("text", "default")
-
-    if text == '':
-        response  = "CON What would you want to check \n"
-        response += "1. My Account \n"
-        response += "2. My phone number"
-
-    elif text == '1':
-        response = "CON Choose account information you want to view \n"
-        response += "1. Account number \n"
-        response += "2. Account balance"
-
-    elif text == '1*1':
-        accountNumber  = "ACC1001"
-        response = "END Your account number is " + accountNumber
-
-    elif text == '1*2':
-        balance  = "KES 10,000"
-        response = "END Your balance is " + balance
-    elif text == '2':
-        response = "END This is your phone number " + phone_number  
-    return response
-
 
 # Helper Functions
 def make_call(phone_number):
@@ -68,16 +33,16 @@ def send_sms(phone_number, message):
     return sms.send(options)
 
 # USSD Endpoint
-@app.route('/USSD', methods=['POST', 'GET'])
-def ussd_callback():
+@app.route('/USSD', methods=['POST'])
+def ussd():
     response = ''
-    session_id = request.values.get("sessionId", None)
-    service_code = request.values.get("serviceCode", None)
-    phone_number = request.values.get("phoneNumber", None)
-    text = request.values.get("text", "default")
+    data = request.get_json()
 
     try:
-        if text == '':
+        phoneNumber = data['phoneNumber']
+        text = data['text']
+
+        if not text:
             response = '''CON Karibu 
             1. Information on wildlife, places to visit
             2. Book ticket
@@ -86,13 +51,13 @@ def ussd_callback():
             5. Give feedback
             6. Helpline'''
         elif text == '1':
-            make_call(phone_number)
+            make_call(phoneNumber)
             response = '''CON You will receive a call shortly \n0. Back'''
         elif text == '2':
-            send_sms(phone_number, "Welcome to Nairobi, the capital of Kenya.")
+            send_sms(phoneNumber, "Welcome to Nairobi, the capital of Kenya.")
             response = '''CON You will receive an SMS with a link to a document about it shortly \n0. Back'''
         elif text == '3':
-            send_sms(phone_number, "BOOK A TOUR WITH US.")
+            send_sms(phoneNumber, "BOOK A TOUR WITH US.")
             response = '''CON You will receive a link to the volunteer form shortly \n0. Back'''
         elif text == '4':
             response = '''CON Thank you \n0. Back'''
@@ -113,4 +78,4 @@ def ussd_callback():
 
 # Start the Server
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=os.environ.get('PORT'))
+    app.run(port=PORT)
--- app1.py
+++ app1.py
@@ -1,14 +1,10 @@
-from flask import Flask, request, Response
-from africastalking.AfricasTalking import AfricasTalking, Voice
+
+# *384*31278#
 import os
-
-# Load environment variables
+from flask import Flask, request
+from africastalking.AfricasTalking import AfricasTalking, Voice
 from dotenv import load_dotenv
-load_dotenv()
 
-# Create Flask App
-app = Flask(__name__)
-PORT = int(os.environ.get('PORT', 3000))
 
 # Configure Africa's Talking credentials
 username = os.environ.get('AT_USERNAME')
@@ -16,6 +12,45 @@ api_key = os.environ.get('AT_API_KEY')
 AT = AfricasTalking(username, api_key)
 sms = AT.Messaging
 voice = Voice
+
+# Load environment variables
+load_dotenv()
+
+# Create Flask App
+app = Flask(__name__)
+PORT = int(os.environ.get('PORT', 3000))
+
+response = ""
+
+@app.route('/', methods=['POST', 'GET'])
+def ussd_callback():
+    global response
+    session_id = request.values.get("sessionId", None)
+    service_code = request.values.get("serviceCode", None)
+    phone_number = request.values.get("phoneNumber", None)
+    text = request.values.get("text", "default")
+
+    if text == '':
+        response  = "CON What would you want to check \n"
+        response += "1. My Account \n"
+        response += "2. My phone number"
+
+    elif text == '1':
+        response = "CON Choose account information you want to view \n"
+        response += "1. Account number \n"
+        response += "2. Account balance"
+
+    elif text == '1*1':
+        accountNumber  = "ACC1001"
+        response = "END Your account number is " + accountNumber
+
+    elif text == '1*2':
+        balance  = "KES 10,000"
+        response = "END Your balance is " + balance
+    elif text == '2':
+        response = "END This is your phone number " + phone_number  
+    return response
+
 
 # Helper Functions
 def make_call(phone_number):
@@ -33,16 +68,16 @@ def send_sms(phone_number, message):
     return sms.send(options)
 
 # USSD Endpoint
-@app.route('/USSD', methods=['POST'])
-def ussd():
+@app.route('/USSD', methods=['POST', 'GET'])
+def ussd_callback():
     response = ''
-    data = request.get_json()
+    session_id = request.values.get("sessionId", None)
+    service_code = request.values.get("serviceCode", None)
+    phone_number = request.values.get("phoneNumber", None)
+    text = request.values.get("text", "default")
 
     try:
-        phoneNumber = data['phoneNumber']
-        text = data['text']
-
-        if not text:
+        if text == '':
             response = '''CON Karibu 
             1. Information on wildlife, places to visit
             2. Book ticket
@@ -51,13 +86,13 @@ def ussd():
             5. Give feedback
             6. Helpline'''
         elif text == '1':
-            make_call(phoneNumber)
+            make_call(phone_number)
             response = '''CON You will receive a call shortly \n0. Back'''
         elif text == '2':
-            send_sms(phoneNumber, "Welcome to Nairobi, the capital of Kenya.")
+            send_sms(phone_number, "Welcome to Nairobi, the capital of Kenya.")
             response = '''CON You will receive an SMS with a link to a document about it shortly \n0. Back'''
         elif text == '3':
-            send_sms(phoneNumber, "BOOK A TOUR WITH US.")
+            send_sms(phone_number, "BOOK A TOUR WITH US.")
             response = '''CON You will receive a link to the volunteer form shortly \n0. Back'''
         elif text == '4':
             response = '''CON Thank you \n0. Back'''
@@ -78,4 +113,4 @@ def ussd():
 
 # Start the Server
 if __name__ == '__main__':
-    app.run(port=PORT)
+    app.run(host="0.0.0.0", port=os.environ.get('PORT'))
