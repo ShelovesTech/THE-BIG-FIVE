6600144a2039aa807857ee7e09fb526072807973
Kevin Isom <47275702+Isomkevin@users.noreply.github.com>
2024-06-05 17:10:06 +0300

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,170 +1,93 @@
+// Required Libraries
 const express = require("express");
 const bodyParser = require("body-parser");
-const axios = require("axios");
-const { async } = require("validate.js");
+const AfricasTalking = require("africastalking");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
 
+// Load environment variables
+dotenv.config();
+
+// Create Express App
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-const PORT = 3000;
-
+// Configure Africa's Talking credentials
 const credentials = {
-  apiKey: "00fb1d32a2f524923222bc8c95c752a3b9bfdde06f37d75a34528997b26030de",
-  username: "eKaranja777",
+  apiKey: process.env.AT_API_KEY,
+  username: process.env.AT_USERNAME,
 };
-const AfricasTalking = require("africastalking")(credentials);
-const sms = AfricasTalking.SMS;
+const AT = AfricasTalking(credentials);
+const sms = AT.SMS;
+const voice = AT.VOICE;
 
+// Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("combined")); // Logger
 
+// Helper Functions
+const makeCall = (phoneNumber) => {
+  const options = {
+    callFrom: process.env.CALL_FROM,
+    callTo: [phoneNumber],
+  };
 
- 
+  voice.call(options).then(console.log).catch(console.log);
+};
 
-app.post("/USSD", async(req, res) => {
-    console.log("hiiii")
-    const { phoneNumber, text } = req.body
-     if (text === "") {
-    console.log(text);
+const sendSms = (phoneNumber, message) => {
+  const options = {
+    to: phoneNumber,
+    message: message,
+  };
+  return sms.send(options);
+};
 
-    response = `CON karibu 
-            1. Information on wildlife , places to visit 
-            2. book ticket
-            3. report a problem
+// USSD Endpoint
+app.post("/USSD", async (req, res) => {
+  let response = "";
+  const { phoneNumber, text } = req.body;
+
+  try {
+    if (!text) {
+      response = `CON Karibu 
+            1. Information on wildlife, places to visit
+            2. Book ticket
+            3. Report a problem
             4. Join support group
-            5. Give feedback 
-            6. HELPLINE
-            `;
-  } else if (text === "1") {
-  
-    const credentials = {
-      apiKey:
-        "",
-      username: "",
-    };
-    const AfricasTalking = require("africastalking")(credentials);
-    const voice = AfricasTalking.VOICE;
-
-    function makeCall(phoneNumber) {
-      const options = {
-        callFrom: "0792190741",
-
-        callTo: [phoneNumber],
-      };
-
-      console.log("calling");
-      voice.call(options).then(console.log).catch(console.log);
+            5. Give feedback
+            6. Helpline`;
+    } else if (text === "1") {
+      makeCall(phoneNumber);
+      response = `CON You will receive a call shortly \n0. Back`;
+    } else if (text === "2") {
+      await sendSms(phoneNumber, "Welcome to Nairobi, the capital of Kenya.");
+      response = `CON You will receive an SMS with a link to a document about it shortly \n0. Back`;
+    } else if (text === "3") {
+      await sendSms(phoneNumber, "BOOK A TOUR WITH US.");
+      response = `CON You will receive a link to the volunteer form shortly \n0. Back`;
+    } else if (text === "4") {
+      response = `CON Thank you \n0. Back`;
+    } else if (text === "1*0" || text === "2*0" || text === "3*0" || text === "4*0") {
+      response = `CON What would you like to do
+            1. Information on wildlife, places to visit
+            2. Book ticket
+            3. Report a problem
+            4. Join support group
+            5. Give feedback
+            6. Helpline`;
     }
-
-    makeCall(phoneNumber);
-
-    response = `CON you will receive a call shortly \n
-    0. Back`;
-  } else if (text === "2") {
-    console.log(phoneNumber)
-    function sendSms() {
-      console.log("wwwww");
-      const options = {
-        to: phoneNumber,
-        message:
-          "welcome to Nairobi the capital of Kenya. "
-      };
-      sms
-        .send(options)
-        .then((response) => {
-          console.log(response);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
-    console.log("bfore")
-    sendSms(phoneNumber);
-    console.log("fore")
-
-    console.log("smssssss");
-    response = `CON you will receive an sms with a link to a document about it shortly \n
-    0. Back`;
-  } else if (text === "3") {
-    function sendSms() {
-      console.log("wwwww");
-      const options = {
-        to: phoneNumber,
-
-        message:
-          "BOOK A TOUR WITH US.",
-      };
-      sms
-        .send(options)
-        .then((response) => {
-          console.log(response);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
-    sendSms(phoneNumber);
-    // Business logic for first level response
-    response = `CON YOU WILL RECEIVE A LINK TO THE VOLUNTEER FORM SHORTLY \n 
-    0. Back`;
-  } else if (text === "4") {
-    console.log("wwwww222");
-
-       response = `CON thank you \n 
-    0. Back`;
+  } catch (error) {
+    console.error(error);
+    response = `END An error occurred. Please try again later.`;
   }
-  else if (text === "1*0") {
-    console.log(text);
 
-    response = `CON What would you like to do
-            1. information on the wildlife , places to visit
-            2. BOOK TICKET 
-            3. report a problem
-            4.join support group
-            5. Give feedback  
-            6.HELPLINE
-            `;
-  } else if (text === "2*0") {
-    console.log(text);
-
-    response = `CON What would you like to do
-    1. information on wildlife , places to visit
-    2. book ticket
-    3. report a problem
-    4.join support group 
-    5. Give feedback 
-    6.HELPLINE
-            `;
-  } else if (text === "3*0") {
-    console.log(text);
-
-    response = `CON What would you like to do
-    1. information on wildlife, places to visit
-    2. book ticket
-    3. report a problem
-    4.join support group 
-    5. Give feedback
-    6.HELPLINE
-           
-            `;
-  } else if (text === "4*0") {
-    //     console.log(text);
-
-    response = `CON What would you like to do
-    1. information on wildlife 
-    2. book ticket
-    3. report a problem
-    4.join support group 
-    5. Give feedback
-    6.HELPLINE
-            `;
-  
-  }
-  // Print the response onto the page so that our gateway can read it
-  res.set("Content-Type: text/plain");
-
+  res.set("Content-Type", "text/plain");
   res.send(response);
 });
 
+// Start the Server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-})
+});
